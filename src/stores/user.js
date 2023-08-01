92c4dbec3901912f0559984c55eb1c642495bd27
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-08-01 16:58:53 +0200

    sanctum OK

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,13 +9,13 @@ export const useUserStore = defineStore('user', {
     },
     actions: {
         async login(credentials) {
-            const token = (await apiRequester.post("/api/sanctum/token", credentials)).data
+            const token = (await apiRequester.post("/api/login", credentials)).data.token
 
             // Use the token for all future requests
             apiRequester.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
             // The user will be available in all our components via `this.$userStore.user` thanks to our custom appPlugin`
-            this.user = (await apiRequester.get("/api/user")).data
+            this.user = (await apiRequester.get("/api/user/info")).data
         }
     }
 })
