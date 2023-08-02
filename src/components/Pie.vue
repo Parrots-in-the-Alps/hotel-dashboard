41d91d62bab731400b679edb202f11a640418d6c
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-08-02 16:44:41 +0200

    add Pie component

---
+++ src/components/Pie.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="chart">
+        <apexchart type="pie" width="380" :options="chartOptions" :series="series" ></apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Pie',
+    data() {
+        return {
+          chartOptions: {
+            chart: {
+              width: 380,
+              type: 'pie',
+            },
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                legend: {
+                  position: 'bottom'
+                }
+              }
+            }]
+          },
+        }
+    },
+    props: {
+        series: {
+          type: Array,
+          required: true,
+        },
+        labels: {
+          type: Array,
+          required: true,
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
