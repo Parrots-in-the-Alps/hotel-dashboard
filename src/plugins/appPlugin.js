bad80d64a2ba4b427572919dcba640b087d04379
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-08-02 14:08:51 +0200

    add apexCharts OK

--- src/plugins/appPlugin.js
+++ src/plugins/appPlugin.js
@@ -1,8 +1,10 @@
 import {useUserStore} from "../stores/user.js"
+import VueApexCharts from "vue3-apexcharts";
 
 export default {
     install(app, options) {
         // Be able to use the store in all components via `this.$userStore`
         app.config.globalProperties.$userStore = useUserStore()
+        app.use(VueApexCharts)
     }
 }
