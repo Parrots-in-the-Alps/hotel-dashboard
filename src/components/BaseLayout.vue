5f7164efd7278802bd2a07a7297907d2d468eed2
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-08-02 10:24:04 +0200

    add header

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,8 +1,30 @@
 <template>
-	<!-- Base layout for all sub pages, put navbar and global elements here-->
+<a-dropdown>
+    <a class="ant-dropdown-link" @click.prevent>
+      Dashboards
+      <DownOutlined />
+    </a>
+  
+      <a-menu>
+        <a-menu-item key="0">
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+            Dashboard stratégique
+          </a>
+        </a-menu-item>
+        <a-menu-item key="1">
+          <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+            Dashboard opérationnel
+          </a>
+        </a-menu-item>
+        <a-menu-item key="3">Dashboard Tactique</a-menu-item>
+      </a-menu>
+ 
+  </a-dropdown>
 	<main>
 		<router-view/>
+		
 	</main>
+	
 </template>
 
 <script>
--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,9 +1,5 @@
 <template>
-	<div>
-		<h1>Home</h1>
-
-		<a-button type="primary">A button</a-button>
-	</div>
+	
 </template>
 
 <script>
