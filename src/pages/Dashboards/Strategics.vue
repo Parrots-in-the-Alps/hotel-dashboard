af5fd2c9005ecc139b10d8ede89d7af34780e0a6
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-08-03 15:05:17 +0200

    WIP: Dashboards Strategics

--- src/pages/Dashboards/Strategics.vue
+++ src/pages/Dashboards/Strategics.vue
@@ -1,14 +1,40 @@
 <template>
-   
-    </template>
-    
-    <script>
-   
-        export default {
-            name: "Strategics"
+    <div>
+        <a-row type="flex" justify="space-around">
+            <a-col :span="14"></a-col>
+            <a-col :span="3">
+                <a-month-picker placeholder="Mois précédent" @change="onChange" />
+            </a-col>
+            <a-col :span="3">
+                <p class="center-text">Par rapport à</p>
+            </a-col>
+            <a-col :span="3">
+                <a-month-picker placeholder="Mois en cours" @change="onChange" />
+            </a-col>
+        </a-row>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Strategics",
+    data() {
+        return {
+
         }
-    </script>
+    },
+    methods: {
+        onChange(date, dateString) {
+            //   console.log(date, dateString);
+        },
+    },
+}
+</script>
     
-    <style scoped>
-    
-    </style>
+<style scoped>
+.center-text {
+    text-align: center
+}
+
+</style>
