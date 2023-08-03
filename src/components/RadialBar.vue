36375906b04276a66139a0ebcf9dc1e7a3ce54fa
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-08-03 11:22:11 +0200

    fix RadialBar component

--- src/components/RadialBar.vue
+++ src/components/RadialBar.vue
@@ -30,32 +30,14 @@
     radialBar: {
       size: undefined,
       inverseOrder: true,
-      hollow: {
-        margin: 5,
-        size: '48%',
-        background: 'transparent',
-
-      },
-      track: {
-        show: false,
-      },
-      startAngle: -180,
-      endAngle: 180
 
     },
+    labels: ['Luxe', 'Standard', 'Suite'],
   },
-  stroke: {
-    lineCap: 'round'
-  },
+
   //remplacer series par le call API 
-  labels: ['Luxe', 'Standard', 'Suite'],
-  legend: {
-    show: true,
-    floating: true,
-    position: 'right',
-    offsetX: 70,
-    offsetY: 240
-  },
+  
+
       }
     },
 
