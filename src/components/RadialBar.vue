b77e97b2decc87dd2abea6cd7693f50d8dc96c2c
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-08-03 11:31:47 +0200

    identation pour MATHIAS

--- src/components/RadialBar.vue
+++ src/components/RadialBar.vue
@@ -1,49 +1,45 @@
 <template>
-	<div>
-	
+  <div>
 
-		<apexchart  type="radialBar" :options="plotOptions" :series="series"></apexchart>
-	  
 
-	  
-	</div>
-	  
+    <apexchart type="radialBar" :options="plotOptions" :series="series"></apexchart>
+
+
+
+  </div>
 </template>
 
 <script>
 
 
-	export default {
-		name: "RadialBar",
-        props: {
-        series: {
-            type: Array,
-            required: true,
-        }
-    },
-		data: function() {
-      return {
-        chart: {
-    type: 'radialBar',
+export default {
+  name: "RadialBar",
+  props: {
+    series: {
+      type: Array,
+      required: true,
+    }
   },
-  plotOptions: {
-    radialBar: {
-      size: undefined,
-      inverseOrder: true,
+  data: function () {
+    return {
+      chart: {
+        type: 'radialBar',
+      },
+      plotOptions: {
+        radialBar: {
+          size: undefined,
+          inverseOrder: true,
 
-    },
-    labels: ['Luxe', 'Standard', 'Suite'],
+        },
+        labels: ['Luxe', 'Standard', 'Suite'],
+      },
+    }
   },
-      }
-    },
 
-		
-		
-	}
-	
+
+
+}
+
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
