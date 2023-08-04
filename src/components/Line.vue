8376c5dba6edcb4e62a5affee8616daa503dafdc
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-08-04 11:39:13 +0200

    update Line and Strategics dashboard

--- src/components/Line.vue
+++ src/components/Line.vue
@@ -1,43 +1,58 @@
 <template>
-    <div id="chart">
-        <apexchart type="line" :options="chartOptions" :series="series"></apexchart>
-    </div>
+  <div id="chart">
+    <apexchart type="line" :height="height" :width="width" :options="chartOptions" :series="series"></apexchart>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Line',
-    data() {
-        return {
-          chartOptions: {
-            stroke: {
-              curve: 'straight'
-            },
-            title: {
-              text: this.title,
-              align: 'left'
-            },
-            grid: {
-              row: {
-                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-                opacity: 0.5
-              },
-            },
-            xaxis: {
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-            }
-          },
-        }
-    },
-    props: {
-        series: {
-            type: Array,
-            required: true,
+  name: 'Line',
+  data() {
+    return {
+      chartOptions: {
+        responsive: [{
+          breakpoint: undefined,
+          options: {},
+        }],
+        colors: this.colors,
+        stroke: {
+          curve: 'straight'
         },
+        title: {
+          text: this.title,
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          },
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        }
+      },
     }
+  },
+  props: {
+    series: {
+      type: Array,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: false,
+    },
+    width: {
+      type: String,
+      required: false,
+    },
+    colors: {
+      type: String,
+      required: false,
+    },
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/Dashboards/Strategics.vue
+++ src/pages/Dashboards/Strategics.vue
@@ -1,17 +1,41 @@
 <template>
-    <div>
-        <a-row type="flex" justify="space-around">
-            <a-col :span="14"></a-col>
-            <a-col :span="3">
-                <a-month-picker placeholder="Mois précédent" @change="onChange" />
-            </a-col>
-            <a-col :span="3">
-                <p class="center-text">Par rapport à</p>
-            </a-col>
-            <a-col :span="3">
-                <a-month-picker placeholder="Mois en cours" @change="onChange" />
-            </a-col>
-        </a-row>
+    <div class="container-column">
+
+        <div class="month-picker mb-25">
+            <a-month-picker class="gap-20 error" placeholder="Mois précédent" @change="onChange" />
+            <p class="gap-20">Par rapport à</p>
+            <a-month-picker class="gap-20 success" placeholder="Mois en cours" @change="onChange" />
+        </div>
+
+        <div class="container-row-evenly mb-25">
+            <Card class="gap-20" title="Temps entre réservation et check-in moyen">
+                <Line :height="'230px'" :width="'450px'" :colors="['#00E396', '#F5222D']" :series="[{ name: 'truc 1' ,data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }, { name: 'truc 2', data: [120, 95, 90, 85, 80, 85, 90, 95, 65] }]"></Line>
+            </Card>
+            <Card class="gap-20" title="Temps d'accueil moyen">
+                <Line :height="'230px'" :width="'450px'" :colors="['#00E396', '#F5222D']" :series="[{ name: 'truc 1', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }, { name: 'truc 2', data: [120, 95, 90, 85, 80, 85, 90, 95, 65] }]"></Line>
+            </Card>
+            <Card class="gap-20" title="Taux de remplissage moyen">
+                <Line :height="'230px'" :width="'450px'" :colors="['#00E396', '#F5222D']" :series="[{ name: 'truc 1', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }, { name: 'truc 2', data: [120, 95, 90, 85, 80, 85, 90, 95, 65] }]"></Line>
+            </Card>
+        </div>
+
+        <div class="container-unique mb-25">
+            <Card class="height" title="Temps de remplissage">
+                <Line :height="'230px'" :width="'1300px'" :colors="['#00E396', '#F5222D']" class="blabla" :series="[{ data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }]"></Line>
+            </Card>
+        </div>
+
+        <!-- TODO: faire des media query pour les width des graphs en longeurs -->
+
+        <div class="container-row-around">
+            <Card title="Temps entre réservation et check-in moyen">
+                <Line :height="'230px'" :width="'450px'" :colors="['#00E396', '#F5222D']" :series="[{ name: 'truc 1', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }, { name: 'truc 2', data: [120, 95, 90, 85, 80, 85, 90, 95, 65] }]"></Line>
+            </Card>
+            <Card title="Temps entre réservation et check-in moyen">
+                <Line :height="'230px'" :width="'450px'" :colors="['#00E396', '#F5222D']" :series="[{ name: 'truc 1', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }, { name: 'truc 2', data: [120, 95, 90, 85, 80, 85, 90, 95, 65] }]"></Line>
+            </Card>
+        </div>
+
     </div>
 </template>
 
@@ -33,8 +57,72 @@ export default {
 </script>
     
 <style scoped>
-.center-text {
-    text-align: center
+.container-unique {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
+.container-column {
+    display: flex;
+    flex-flow: column wrap;
+}
+
+.container-row-around {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+}
+
+.container-row-evenly {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+}
+
+
+.month-picker {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.gap-20 {
+    margin-left: 20px;
+}
+
+.mb-25 {
+    margin-bottom: 25px;
+}
+
+.error {
+    border-color: #00E396;
+    border-inline-end-width: 1px;
+    box-shadow: 0 0 0 2px rgba(255,38,5,.06);
+    outline: 0;
+}
+
+.success {
+    border-color: #F5222D;
+    border-inline-end-width: 1px;
+    box-shadow: 0 0 0 2px rgba(255,38,5,.06);
+    outline: 0;
+}
+
+::placeholder {
+    color: black;
+    opacity: 1;
+}
+
+@media only screen and (max-width: 768px) {
+    .month-picker {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+
+p {
+    margin-bottom: 0px;
+}
 </style>
